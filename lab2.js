152ed94e8b7b133a481a46836fa89b35483eae26
kmr54 <70987704+kmr54@users.noreply.github.com>
2021-02-03 13:34:49 -0500

    Update lab2.js

--- lab2.js
+++ lab2.js
@@ -1,3 +1,22 @@
 function modifyInto(){
 	document.getElementById("p1").innerHTML = "Cats are better!";
 }
+
+function validateInput(){
+var int = document.getElementById("int").value;
+bool = "true";
+
+if(int<=10 || int>=15){
+alert("Value must be greater than 10, less than 15");
+bool="false";
+}
+else{
+alert("The perfect amount of cats!");
+bool="true";
+}
+
+if(bool="false"){
+return false;
+}
+
+}
