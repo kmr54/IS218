06eb16cf98b426ebdde9eeee00c7657412b982d7
kmr54 <70987704+kmr54@users.noreply.github.com>
2021-02-08 19:27:03 -0500

    Create homework2.js

---
+++ homework2.js
@@ -0,0 +1,24 @@
+function validateInput(){
+	    var firstname = document.forms["SubmitForm"]["firstname"]; 
+		var lastname = document.forms["SubmitForm"]["lastname"];
+        var email = document.forms["SubmitForm"]["email"]; 
+        
+        if (firstname.value == "") { 
+            window.alert("Please enter your first name."); 
+            firstname.focus(); 
+            return false; 
+        } 
+  
+        if (lastname.value == "") { 
+            window.alert("Please enter your last name."); 
+            lastname.focus(); 
+            return false; 
+        } 
+  
+        if (email.value == "") { 
+            window.alert("Please enter a valid e-mail address."); 
+            email.focus(); 
+            return false; 
+        } 
+		return true;
+}
