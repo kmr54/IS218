860f54eb991d90aa8f89e0f6a9933c81f33ff836
kmr54 <70987704+kmr54@users.noreply.github.com>
2021-02-08 22:54:10 -0500

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -1,31 +1,36 @@
+//source for regex code: https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
 function validateInput(){
-	    var firstname = document.forms["SubmitForm"]["firstname"]; 
-		var lastname = document.forms["SubmitForm"]["lastname"];
-        var email = document.forms["SubmitForm"]["email"]; 
+      var firstname = document.getElementById("firstname");
+      var lastname = document.getElementById("lastname");
+      var email = document.getElementById("email"); 
+      var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+      var hasNumber = /\d/;
         
         if (firstname.value == "") { 
             window.alert("Please enter your first name."); 
-            firstname.focus(); 
             return false; 
         } 
+		
   
         if (lastname.value == "") { 
             window.alert("Please enter your last name."); 
-            lastname.focus(); 
             return false; 
         } 
-  
-        if (email.value == "") { 
-            window.alert("Please enter a valid e-mail address."); 
-            email.focus(); 
+        
+  	if (email.value == "") { 
+            window.alert("Please enter your email."); 
             return false; 
         } 
-		return true;
+       
+      if(hasNumber.test("firstname")==true || hasNumber.test("lastname")==true){
+      	return false;       
+       }
+       
+      if ( !(input.value.match(validRegex)) ) {
+	      alert("Invalid email address!"); 
+	      return false; 
+      }
+      
+      return true;
 }
-
-function resetForm(){  
-   document.getElementById("SubmitForm").reset();  
- } 
- function submitForm(){  
-	document.getElementById("SubmitForm").submit();
- }
+ 
